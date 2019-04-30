0586fde48e9dabd8dfe4a78d365d4e33e4cb92aa
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-04-30 21:49:01 +0000

    Initialized

---
+++ EIEvent/trunk/inst/conf/EIini.R
@@ -0,0 +1,25 @@
+## These are application generic parameters
+EIeng.common <- list(host="localhost",username="EI",password="secret",
+                     dbname="EIRecords",P4dbname="Proc4",waittime=.25)
+appstem <- basename(app)
+
+## These are for application specific parameters
+EIeng.params <- list(app=app)
+
+
+logfile <- file.path("/usr/local/share/Proc4/logs",
+                     paste("EI_",appstem,".log",sep=""))
+
+data2json <- function(dat) {
+  toJSON(sapply(dat,unboxer))
+}
+
+EI.listenerSpecs <-
+  list("InjectionListener"=list(sender=paste("EI",appstem,sep="_"),
+            dbname="EARecords",dburi="mongodb://localhost",
+            colname="Observables",messSet="New Observables"),
+       "UpdateListener"=list(dbname="Proc4",dburi="mongodb://localhost",
+            colname="Players",targetField="data",
+            messSet=c("Money Earned","Money Spent"),
+            jsonEncoder="data2json"))
+
